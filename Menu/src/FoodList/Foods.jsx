639dfd0952ce294bb3adba729628439b54fb1a90
Nahom4748 <155730526+Nahom4748@users.noreply.github.com>
2024-06-17 22:03:36 +0300

    Add css

--- Menu/src/FoodList/Foods.jsx
+++ Menu/src/FoodList/Foods.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import "../components/style.css";
-import FoodList from "./FoodList";
+import Food from "./FoodList";
 import RenderFood from "./RenderFood";
 import img from "../img/Burger.png";
 
@@ -11,8 +11,7 @@ class Foods extends Component {
       <div>
         <div className="title">Restaurant Menu </div>
         <div className="outer">
-          {FoodList.map((element, i) => {
-            console.log(element);
+          {Food.map((element, i) => {
             return <RenderFood Dataaa={element} key={i} />;
           })}
         </div>
